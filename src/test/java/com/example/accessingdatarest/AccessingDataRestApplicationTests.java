38ab3328df6b6a37689e9f0e2f333ce6c7a82f80
Alexey Anosov <a.anosov15@yandex.ru>
2022-06-26 01:47:37 +0500

    build: изменена конфигурация allure

--- src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
+++ src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
@@ -1,12 +1,20 @@
 package com.example.accessingdatarest;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Epic("Login Tests Epic")
+@Feature("Invalid Login Features")
 class AccessingDataRestApplicationTests {
 
 	@Test
+	@Story("User tries to login the system with empty username and invalid password.")
+	@Description("Invalid Login Test with Empty Username and Invalid Password.")
 	void contextLoads() {
 	}
 
